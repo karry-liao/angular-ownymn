eb726d9e1b18b7d075ac2563526804de02c76d4c
karry-liao <1441534373@qq.com>
2021-06-09 15:00:40 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ownymn
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ownymn)
---
+++ src/app/product-alerts/product-alerts.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { Input, Output, EventEmitter } from '@angular/core';
+@Component({
+  selector: 'app-product-alerts',
+  templateUrl: './product-alerts.component.html',
+  styleUrls: ['./product-alerts.component.css']
+})
+export class ProductAlertsComponent implements OnInit {
+  @Input() product: any;
+  @Output() notify = new EventEmitter();
+  constructor() {}
+
+  ngOnInit() {}
+}
